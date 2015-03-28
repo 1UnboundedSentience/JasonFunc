87516d2b0cf126aae5de1193bf12522e4aea293e
1UnboundedSentience <jsnx21@gmail.com>
2015-03-27 21:10:24 -0700

    small change of <= to <

--- mySplicer.js
+++ mySplicer.js
@@ -1,7 +1,7 @@
 function mySplicer(array, index, size) {
     array2 = [];
     
-    for (i = 0; i<=index; i++) {
+    for (i = 0; i<index; i++) {
         array2.push(array[i]);
     }
     for (i = index+size ; i<array.length; i++) {
@@ -14,3 +14,4 @@ function mySplicer(array, index, size) {
 }
 
 mySplicer([1,2,2,3,3,3,4], 2, 3);
+
